40d9617508ffe49b5e986b6e07ce7a8d7d7f71e1
Igors Dubanevics <igors.dubanevics@york.ac.uk>
2021-10-01 11:38:49 +0300

    Run tleap for protein/ligand complex

--- src/00-structure/03.configure_ligand.sh
+++ src/00-structure/03.configure_ligand.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# Creates force field modification for ligand
+# Creates force field modification for the ligand
 
 PDB_RAW_DIR="pdb/00-raw"
 PDB_INT_DIR="pdb/01-interim"
 PDB_PRO_DIR="pdb/02-processed"
-TMP_DIR="tmp"
+ANTECHAM_DIR="tmp"
 
 # Ligand name: cAMP
 # Ligand charge: -1
-antechamber -i ${PDB_PRO_DIR}/ligand.pdb -fi pdb -o ${PDB_INT_DIR}/ligand.mol2 -fo mol2 \
-    -s 2 -c bcc -at gaff2 -nc -1 -m 2
-
+# Multiplicity: 2
+antechamber -i ${PDB_PRO_DIR}/ligand.pdb -fi pdb -o ${ANTECHAM_DIR}/ligand.mol2 -fo mol2 \
+    -s 2 -c bcc -at gaff2 \
+    -nc -1 \
+    -m 2
 rm ANTECHAMBER_*.AC ANTECHAMBER_*.AC0 ATOMTYPE.INF sqm.*
 
-parmchk2 -i ${PDB_INT_DIR}/ligand.mol2 -f mol2 -o ${TMP_DIR}/ligand.frcmod
+parmchk2 -i ${ANTECHAM_DIR}/ligand.mol2 -f mol2 -o ${ANTECHAM_DIR}/ligand.frcmod -s gaff2
