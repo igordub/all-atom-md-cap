472b3e0a6d6ad755199c9e39972b528bc3309226
Igors Dubanevics <igors.dubanevics@york.ac.uk>
2021-10-01 12:33:06 +0300

    Add second ligand to protein

--- src/00-structure/02.prepare_pdb.sh
+++ src/00-structure/02.prepare_pdb.sh
@@ -14,8 +14,11 @@ pdb4amber -i ${PDB_RAW_DIR}/complex.pdb -o ${PDB_INT_DIR}/complex.pdb --dry --re
 grep -e '^ATOM\|^TER\|^END' ${PDB_INT_DIR}/complex.pdb > ${PDB_INT_DIR}/protein.pdb
 grep -e '^HETATM' ${PDB_INT_DIR}/complex.pdb > ${PDB_INT_DIR}/ligand.pdb
 
-
 cp ${PDB_INT_DIR}/complex.pdb ${PDB_PRO_DIR}/complex.pdb
 cp ${PDB_INT_DIR}/protein.pdb ${PDB_PRO_DIR}/protein.pdb
-# Extract only one ligand from chain A
-grep -e '^HETATM.\{14\} A ' ${PDB_INT_DIR}/ligand.pdb > ${PDB_PRO_DIR}/ligand.pdb
+# Two identical ligands on each chain
+cp ${PDB_INT_DIR}/ligand.pdb ${PDB_PRO_DIR}/ligands.pdb
+
+# Extract ligand separately
+grep -e '^HETATM.\{14\} A ' ${PDB_INT_DIR}/ligand.pdb > ${PDB_PRO_DIR}/ligand.chainA.pdb
+grep -e '^HETATM.\{14\} B ' ${PDB_INT_DIR}/ligand.pdb > ${PDB_PRO_DIR}/ligand.chainB.pdb
--- src/00-structure/03.configure_ligand.sh
+++ src/00-structure/03.configure_ligand.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
 # Creates force field modification for the ligand
+# Each ligand must be treated separately
 
 PDB_RAW_DIR="pdb/00-raw"
 PDB_INT_DIR="pdb/01-interim"
 PDB_PRO_DIR="pdb/02-processed"
-ANTECHAM_DIR="tmp"
+ANTECHAM_DIR="antechamber"
 
 # Ligand name: cAMP
 # Ligand charge: -1
 # Multiplicity: 2
-antechamber -i ${PDB_PRO_DIR}/ligand.pdb -fi pdb -o ${ANTECHAM_DIR}/ligand.mol2 -fo mol2 \
+
+# Run antechamber 
+antechamber -i ${PDB_PRO_DIR}/ligand.chainA.pdb -fi pdb -o ${ANTECHAM_DIR}/ligand.chainA.mol2 -fo mol2 \
     -s 2 -c bcc -at gaff2 \
     -nc -1 \
     -m 2
 rm ANTECHAMBER_*.AC ANTECHAMBER_*.AC0 ATOMTYPE.INF sqm.*
 
-parmchk2 -i ${ANTECHAM_DIR}/ligand.mol2 -f mol2 -o ${ANTECHAM_DIR}/ligand.frcmod -s gaff2
+antechamber -i ${PDB_PRO_DIR}/ligand.chainB.pdb -fi pdb -o ${ANTECHAM_DIR}/ligand.chainB.mol2 -fo mol2 \
+    -s 2 -c bcc -at gaff2 \
+    -nc -1 \
+    -m 2
+rm ANTECHAMBER_*.AC ANTECHAMBER_*.AC0 ATOMTYPE.INF sqm.*
+
+# Run parmch2
+parmchk2 -i ${ANTECHAM_DIR}/ligand.chainA.mol2 -f mol2 -o ${ANTECHAM_DIR}/ligand.chainA.frcmod -s gaff2
+parmchk2 -i ${ANTECHAM_DIR}/ligand.chainB.mol2 -f mol2 -o ${ANTECHAM_DIR}/ligand.chainB.frcmod -s gaff2
