d7a4def95089553ac4f863650f275321a288ec50
Igors Dubanevics <igors.dubanevics@york.ac.uk>
2021-10-01 10:38:24 +0300

    Configure ligand's force field

--- src/00-structure/03.configure_ligand.sh
+++ src/00-structure/03.configure_ligand.sh
@@ -3,11 +3,15 @@
 # Creates force field modification for ligand
 
 PDB_RAW_DIR="pdb/00-raw"
-PDB_INT_DIR="pdb/02-interim"
-PDB_PRO_DIR="pdb/03-processed"
+PDB_INT_DIR="pdb/01-interim"
+PDB_PRO_DIR="pdb/02-processed"
+TMP_DIR="tmp"
 
-# Ligand name: VIR251
-# Ligand charge: 0
+# Ligand name: cAMP
+# Ligand charge: -1
 antechamber -i ${PDB_PRO_DIR}/ligand.pdb -fi pdb -o ${PDB_INT_DIR}/ligand.mol2 -fo mol2 \
-    -s 2 -c bcc -at gaff2 -nc 0 -m 1 -eq 2
-parmchk2 -i ${PDB_INT_DIR}/ligand.mol2 -f mol2 -o ${PDB_PRO_DIR}/ligand.frcmod
+    -s 2 -c bcc -at gaff2 -nc -1 -m 2
+
+rm ANTECHAMBER_*.AC ANTECHAMBER_*.AC0 ATOMTYPE.INF sqm.*
+
+parmchk2 -i ${PDB_INT_DIR}/ligand.mol2 -f mol2 -o ${TMP_DIR}/ligand.frcmod
